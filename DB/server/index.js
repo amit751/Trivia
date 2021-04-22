a1c972701f46e574b42d2f87d970024d9f9757dc
DannyIsa <daniisakov@gmail.com>
2021-04-22 15:28:47 +0300

    small changes

--- DB/server/index.js
+++ DB/server/index.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const app = express();
 const { All_Data } = require("../models");
 const { Questions } = require("../models");
 const { Op } = require("sequelize");
@@ -5,3 +7,7 @@ const { Op } = require("sequelize");
 All_Data.findAll({})
   .then((results) => console.log(results.map((result) => result.toJSON())))
   .catch((e) => console.log(e));
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
