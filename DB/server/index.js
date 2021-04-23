e2ec62e08628ef7cc4be3af778b0f40111bb15a2
amit <amitoosh.mor@gmail.com>
2021-04-23 10:26:35 +0300

    working on first api
    get country function works

--- DB/server/index.js
+++ DB/server/index.js
@@ -1,42 +1,15 @@
+
+//imports
 const express = require("express");
 const app = express();
-const {
-  Saved_Questions,
-  All_Data,
-  Questions,
-  Players,
-  sequelize,
-} = require("../models");
+const {Saved_Questions,All_Data,Questions,Players,} = require("../models");
 const { Op, literal } = require("sequelize");
+
+
+
+
+//functions
 const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-
-app.get("/newQuestion", async (req, res) => {
-  const randomId = random(1, 21);
-  const newQ = await Questions.findOne({
-    where: {
-      Id: randomId,
-    },
-  });
-  // let questionString = "";
-  switch (newQ.type) {
-    case 1:
-  }
-  let numberOfOptions;
-  if (newQ.type === 1 || newQ.type === 2) {
-    numberOfOptions = 4;
-  } else {
-    numberOfOptions = 2;
-  }
-
-  const options = await getCountryFromData(
-    newQ["table_column"],
-    numberOfOptions
-  );
-  const allGivenData = [];
-  allGivenData.push(options);
-  allGivenData.push(newQ);
-  return res.json(allGivenData);
-});
 
 function getCountryFromData(column, numberOfOptions) {
   return All_Data.findAll({
@@ -55,6 +28,13 @@ function getCountryFromData(column, numberOfOptions) {
     });
 }
 
+
+
+
+
+
+
+///api
 //GET: /newQ => generate a new q
 // get: /savedQ => send a saved one
 // post: /savedQ => post question
@@ -62,11 +42,31 @@ function getCountryFromData(column, numberOfOptions) {
 // get: /player=>
 // post: /player => score and player name after game round
 
-//*bonus:
 
-// All_Data.findAll({})
-//   .then((results) => console.log(results.map((result) => result.toJSON())))
-//   .catch((e) => console.log(e));
+app.get("/newQuestion", async (req, res) => {
+  
+  const newQ = await Questions.findOne({
+    order: literal("rand()"),
+    limit: 1,
+  });
+
+  let numberOfOptions;
+  if (newQ.type === 1 || newQ.type === 2) {
+    numberOfOptions = 4;
+  } else {
+    numberOfOptions = 2;
+  }
+
+  const options = await getCountryFromData(
+    newQ["table_column"],
+    numberOfOptions
+  );
+  const allGivenData = [];
+  allGivenData.push(options);
+  allGivenData.push(newQ);
+  return res.json(allGivenData);
+});
+
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
