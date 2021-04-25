e055284dd6f65f034023f48f1064a8afaea7b084
amit <amitoosh.mor@gmail.com>
2021-04-26 01:50:54 +0300

    need to style game and change colors

--- client/src/component/welcomePage.js
+++ client/src/component/welcomePage.js
@@ -6,23 +6,29 @@ import {
   Switch,
   Redirect,
 } from "react-router-dom";
-// import "../style/Welcome.css";
+import "../style/Welcome.css";
 
 export default function WelcomePage({ setPlayerName }) {
   const input = useRef();
   return (
-    <div id="welcome-component">
-      <input ref={input} id="player-name" type="text"></input>
-      <Link className="link" to="/Game">
-        <button
-          onClick={() => {
-            setPlayerName(input.current.value);
-          }}
-        >
-          START GAME
-        </button>
-      </Link>
-      <Link to="/TableScore">Table score</Link>
-    </div>
+    <>
+      <h1 className="headline">trivia around the world</h1>
+      <div id="welcome-component">
+        <Link id="table-link" to="/TableScore">
+          Table score
+        </Link>
+        <span id="name">name:</span>
+        <input ref={input} id="player-name" type="text"></input>
+        <Link id="game-link" className="link" to="/Game">
+          <button
+            onClick={() => {
+              setPlayerName(input.current.value);
+            }}
+          >
+            START GAME
+          </button>
+        </Link>
+      </div>
+    </>
   );
 }
